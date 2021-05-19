0916f0fdd308a9d241f16e4841c80bf958833890
Mohan Chinnappan <mohan.chinnappan.n@gmail.com>
2021-05-19 10:32:32 -0400

    tcrm update

--- src/code/c4/kovai/lwc/main/default/lwc/tCRMHelloWorld/tCRMHelloWorld.js
+++ src/code/c4/kovai/lwc/main/default/lwc/tCRMHelloWorld/tCRMHelloWorld.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class TCRMHelloWorld extends LightningElement {}
+export default class TCRMHelloWorld extends LightningElement {
+
+    @api title;
+}
